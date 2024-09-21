323defa492dbf08d9efdf0bdc52f1e42380247cb
Ankush_Kapoor <work.ankushkapoor1626@gmail.com>
2024-09-21 17:50:27 +0530

    Add files via upload

---
+++ src/editor.py
@@ -0,0 +1,220 @@
+from PyQt5.QtCore import *
+from PyQt5.Qsci import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtSvg import QSvgWidget
+from PyQt5.QtGui import *
+
+from pathlib import Path
+import subprocess  # Import subprocess for running commands
+import resources
+import os
+import platform
+
+from lexer import PyCustomLexer
+from autocompleter import AutoCompleter
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from main import MainWindow
+
+class Editor(QsciScintilla):
+    def __init__(self, main_window, parent=None, path: Path = None, is_python_file=True):
+        super(Editor, self).__init__(parent)
+
+        self.main_window: MainWindow = main_window
+        self._current_file_changed = False
+        self.first_launch = True
+
+        self.path = path
+        self.is_python_file = is_python_file
+
+        # Connect the textChanged signal to track changes
+        self.textChanged.connect(self._textChanged)
+
+        # Font setup
+        self.window_font = parent.window_font if parent else QFont("Consolas", 12)
+        self.setFont(self.window_font)
+
+        self.setUtf8(True)
+        self.setBraceMatching(QsciScintilla.SloppyBraceMatch)
+        self.setIndentationGuides(True)
+        self.setTabWidth(4)
+        self.setIndentationsUseTabs(False)
+        self.setAutoIndent(True)
+
+        self.cursorPositionChanged.connect(self._cursorPositionChanged)
+
+        # Caret setup
+        self.setCaretForegroundColor(QColor("#ffffff"))
+        self.setCaretLineVisible(True)
+        self.setCaretWidth(2)
+        self.setCaretLineBackgroundColor(QColor("#3b3a3a"))
+
+        # EOL setup
+        self.setEolMode(QsciScintilla.EolWindows)
+        self.setEolVisibility(False)
+
+        # Autocomplete setup
+        self.setAutoCompletionSource(QsciScintilla.AcsAll)
+        self.setAutoCompletionThreshold(1)
+        self.setAutoCompletionCaseSensitivity(False)
+        self.setAutoCompletionUseSingle(QsciScintilla.AcusNever)
+
+        if self.is_python_file:
+            self.setup_python_lexer()
+        else:
+            self.setPaper(QColor("#1f1f1f"))
+            self.setColor(QColor("#abb2bf"))
+
+        # Line numbers
+        self.setMarginType(0, QsciScintilla.NumberMargin)
+        self.setMarginWidth(0, "00000")
+        self.setMarginsForegroundColor(QColor("#626e82"))
+        self.setMarginsBackgroundColor(QColor("#1f1f1f"))
+
+        self.keyPressEvent = self.keyPressEvent
+
+        self.svg_icon = QSvgWidget("./src/icons/run.svg")  # Replace with your SVG file path
+        self.svg_icon.setFixedSize(60, 60)  # Set size as needed
+        self.svg_icon.setAttribute(Qt.WA_TransparentForMouseEvents, False)
+
+        # Create a layout to place the SVG in the top right corner
+        layout = QVBoxLayout(self)
+        layout.addWidget(self.svg_icon, alignment=Qt.AlignTop | Qt.AlignRight)
+
+        # Connect the SVG icon click to run_code
+        self.svg_icon.mousePressEvent = self.svg_icon_clicked
+
+    def setup_python_lexer(self):
+        self.lexer = PyCustomLexer(self)
+        self.lexer.setDefaultFont(self.window_font)
+
+        # Autocomplete API setup
+        self._api = QsciAPIs(self.lexer)
+        self.auto_completer = AutoCompleter(self.path, self._api)
+        self.auto_completer.finished.connect(self.loaded_autocomplete)
+
+        self.setLexer(self.lexer)
+
+    @property
+    def current_file_changed(self):
+        return self._current_file_changed
+
+    @current_file_changed.setter
+    def current_file_changed(self, value: bool):
+        curr_index = self.main_window.tab_view.currentIndex()
+        if value:
+            self.main_window.tab_view.setTabText(curr_index, "*"+self.path.name)
+            self.main_window.setWindowTitle(f"*{self.path.name} - {self.main_window.app_name}")
+        else:
+            if self.main_window.tab_view.tabText(curr_index).startswith("*"):
+                self.main_window.tab_view.setTabText(
+                    curr_index,
+                    self.main_window.tab_view.tabText(curr_index)[1:]
+                )
+                self.main_window.setWindowTitle(self.main_window.windowTitle()[1:])
+
+        self._current_file_changed = value
+
+    def svg_icon_clicked(self, event):
+        if event.button() == Qt.LeftButton:
+            self.run_code()  # Call the run_code method
+
+    def toggle_comment(self, text: str):
+        lines = text.split('\n')
+        toggled_lines = []
+        for line in lines:
+            if line.startswith('#'):
+                toggled_lines.append(line[1:].lstrip())
+            else:
+                toggled_lines.append("# " + line)
+
+        return '\n'.join(toggled_lines)
+
+    def keyPressEvent(self, e: QKeyEvent) -> None:
+        if e.modifiers() == Qt.ControlModifier and e.key() == Qt.Key_Space:
+            if self.is_python_file:
+                pos = self.getCursorPosition()
+                self.auto_completer.get_completions(pos[0]+1, pos[1], self.text())
+                self.autoCompleteFromAPIs()
+                return
+
+        if e.modifiers() == Qt.ControlModifier and e.key() == Qt.Key_X:  # CUT SHORTCUT
+            if not self.hasSelectedText():
+                line, index = self.getCursorPosition()
+                self.setSelection(line, 0, line, self.lineLength(line))
+                self.cut()
+                return
+
+        if e.modifiers() == Qt.ControlModifier and e.text() == "/":  # COMMENT SHORTCUT
+            if self.hasSelectedText():
+                start, srow, end, erow = self.getSelection()
+                self.setSelection(start, 0, end, self.lineLength(end)-1)
+                self.replaceSelectedText(self.toggle_comment(self.selectedText()))
+                self.setSelection(start, srow, end, erow)
+            else:
+                line, _ = self.getCursorPosition()
+                self.setSelection(line, 0, line, self.lineLength(line)-1)
+                self.replaceSelectedText(self.toggle_comment(self.selectedText()))
+                self.setSelection(-1, -1, -1, -1)  # reset selection
+            return
+
+        if e.modifiers() == Qt.ControlModifier and (e.key() == Qt.Key_Return or e.key() == Qt.Key_Enter):
+            current_line, current_index = self.getCursorPosition()
+            self.insertAt("\n", current_line + 1, 0)
+            self.setCursorPosition(current_line + 1, 0)
+            return
+
+        if e.modifiers() == Qt.ControlModifier and e.key() == Qt.Key_W:
+            if self.main_window.tab_view.count() == 0:
+                self.main_window.close()
+            else:
+                current_index = self.main_window.tab_view.currentIndex()
+                self.main_window.close_tab(current_index)
+            return
+
+        return super().keyPressEvent(e)
+
+    def _cursorPositionChanged(self, line: int, index: int) -> None:
+        if self.is_python_file:
+            self.auto_completer.get_completions(line + 1, index, self.text())
+
+    def loaded_autocomplete(self):
+        pass
+
+    def _textChanged(self):
+        # Check if the file is not saved and it's not the first launch
+        if not self.current_file_changed and not self.first_launch:
+            self.current_file_changed = True  # This will trigger the property setter
+        if self.first_launch:
+            self.first_launch = False  # Mark the first launch as done
+
+    def run_code(self):
+        # Ensure the file is saved before running
+        if self.path is not None:
+            # Save the file if it has unsaved changes
+            if self.current_file_changed:
+                self.save_file()
+
+            file_path = str(self.path)
+            folder_path = os.path.dirname(file_path)  # Get the folder containing the file
+
+            try:
+                # Only handling Windows
+                if platform.system() == "Windows":
+                    # Ensure we get absolute paths
+                    folder_path = os.path.abspath(folder_path)
+                    file_name = os.path.basename(file_path)
+
+                    # Command to change directory and run the Python file
+                    cmd = f'cd "{folder_path}" && python "{file_name}"'
+                    
+                    # Run the command in a new cmd window
+                subprocess.Popen(
+                    ["cmd.exe", "/k", "cd", folder_path, "&&", "python", file_name],
+                    creationflags=subprocess.CREATE_NEW_CONSOLE
+                )
+            except Exception as e:
+                QMessageBox.critical(self, "Run Error", f"Failed to run the file: {e}")
+        else:
+            QMessageBox.warning(self, "No File", "Please save the file before running.")
---
+++ src/main.py
@@ -0,0 +1,437 @@
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.Qsci import * 
+
+from pathlib import Path
+import sys
+import os
+import subprocess
+
+from editor import Editor
+from file_manager import FileManager
+from autocompleter import AutoCompleter
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(QMainWindow, self).__init__()
+
+        self.side_bar_clr = "#181818"
+
+        self.init_ui()
+
+        self.current_file = None
+        self.current_side_bar = None
+
+    def init_ui(self):
+        self.app_name = "CodeNexus - Code Editor"
+        self.setWindowTitle(self.app_name)
+
+        # Set the window icon
+        self.setWindowIcon(QIcon("./src/icons/icon.svg"))
+
+        self.resize(1300, 900)
+
+        self.setStyleSheet(open("./src/css/style.qss", "r").read())
+
+
+        self.window_font = QFont("Consolas")
+        self.window_font.setPointSize(12)
+        self.setFont(self.window_font)
+
+
+        self.set_up_menu()
+        self.set_up_body()
+        self.set_up_status_bar()
+
+        self.show()
+
+    def set_up_status_bar(self):
+        # Create status bar
+        stat = QStatusBar(self)
+        stat.setStyleSheet("color: #D3D3D3;")
+        stat.showMessage("Ready", 3000)
+        self.setStatusBar(stat)
+
+    def set_up_menu(self):
+        menu_bar = self.menuBar()
+
+        # File Menu
+        file_menu = menu_bar.addMenu("File")
+        
+        new_file = file_menu.addAction("New")
+        new_file.setShortcut("Ctrl+N")
+        new_file.triggered.connect(self.new_file)
+
+        file_menu.addSeparator()
+
+        open_file = file_menu.addAction("Open File")
+        open_file.setShortcut("Ctrl+O")
+        open_file.triggered.connect(self.open_file)
+
+        open_folder = file_menu.addAction("Open Folder")
+        open_folder.setShortcut("Ctrl+K")
+        open_folder.triggered.connect(self.open_folder)
+
+        file_menu.addSeparator()
+        
+        save_file = file_menu.addAction("Save")
+        save_file.setShortcut("Ctrl+S")
+        save_file.triggered.connect(self.save_file)
+
+        save_as = file_menu.addAction("Save As")
+        save_as.setShortcut("Ctrl+Shift+S")
+        save_as.triggered.connect(self.save_as)
+
+        file_menu.addSeparator()
+
+        run_action = file_menu.addAction("Run")
+        save_as.setShortcut("Ctrl+R")
+        run_action.triggered.connect(self.run_current_file)  # Connect to the run function
+
+        file_menu.addSeparator()
+
+        # Exit option
+        exit_action = file_menu.addAction("Exit")
+        exit_action.triggered.connect(self.close_application)
+        
+        # Edit menu
+        edit_menu = menu_bar.addMenu("Edit")
+
+        undo_action = edit_menu.addAction("Undo")
+        undo_action.setShortcut("Ctrl+Z")
+        undo_action.triggered.connect(self.undo)
+
+        redo_action = edit_menu.addAction("Redo")
+        redo_action.setShortcut("Ctrl+Y")
+        redo_action.triggered.connect(self.redo)
+        
+        edit_menu.addSeparator()
+
+        cut_action = edit_menu.addAction("Cut")
+        cut_action.setShortcut("Ctrl+X")
+        cut_action.triggered.connect(self.cut)
+
+        copy_action = edit_menu.addAction("Copy")
+        copy_action.setShortcut("Ctrl+C")
+        copy_action.triggered.connect(self.copy)
+
+        paste_action = edit_menu.addAction("Paste")
+        paste_action.setShortcut("Ctrl+V")
+        paste_action.triggered.connect(self.paste)
+
+    def get_editor(self, path: Path = None, is_python_file=True) -> QsciScintilla:
+        editor = Editor(self, path=path, is_python_file=is_python_file)
+        return editor
+
+    def is_binary(self, path):
+        '''
+        Check if file is binary
+        '''
+        with open(path, 'rb') as f:
+            return b'\0' in f.read(1024)
+
+    def set_new_tab(self, path: Path, is_new_file=False):
+        if not is_new_file and self.is_binary(path):
+            self.statusBar().showMessage("Cannot Open Binary File", 2000)
+            return
+
+        if path.is_dir():
+            return
+
+        # add whichever extentions you consider as python file
+        editor = self.get_editor(path, path.suffix in {".py", ".pyw"}) 
+        
+        if is_new_file:
+            self.tab_view.addTab(editor, "untitled")
+            self.setWindowTitle("untitled - " + self.app_name)
+            self.statusBar().showMessage("Opened untitled")
+            self.tab_view.setCurrentIndex(self.tab_view.count() - 1)
+            self.current_file = None
+            return
+        
+        # check if file already open
+        for i in range(self.tab_view.count()):
+            if self.tab_view.tabText(i) == path.name or self.tab_view.tabText(i) == "*"+path.name:
+                self.tab_view.setCurrentIndex(i)
+                self.current_file = path
+                return
+
+        # create new tab
+        self.tab_view.addTab(editor, path.name)
+        editor.setText(path.read_text(encoding="utf-8"))
+        self.setWindowTitle(f"{path.name} - {self.app_name}")
+        self.current_file = path
+        self.tab_view.setCurrentIndex(self.tab_view.count() - 1)
+        self.statusBar().showMessage(f"Opened {path.name}", 2000)
+
+    def set_cursor_pointer(self, e):
+        self.setCursor(Qt.PointingHandCursor)
+
+    def set_cursor_arrow(self, e):
+        self.setCursor(Qt.ArrowCursor)
+
+    def get_side_bar_label(self, path, name):
+        label = QLabel()
+        label.setPixmap(QPixmap(path).scaled(QSize(30, 30)))
+        label.setAlignment(Qt.AlignmentFlag.AlignTop)
+        label.setFont(self.window_font)
+        label.mousePressEvent = lambda e: self.show_hide_tab(e, name)
+        # Chaning Cursor on hover
+        label.enterEvent = self.set_cursor_pointer
+        label.leaveEvent = self.set_cursor_arrow
+        return label
+
+    def get_frame(self) -> QFrame:
+        frame = QFrame()
+        frame.setFrameShape(QFrame.NoFrame)
+        frame.setFrameShadow(QFrame.Plain)
+        frame.setContentsMargins(0, 0, 0, 0)
+        frame.setStyleSheet('''
+            QFrame {
+                background-color: #181818;
+                border-radius: 5px;
+                border: none;
+                padding: 5px;
+                color: #D3D3D3;  
+            }
+            QFrame:hover {
+                color: white;
+            }
+        ''')
+        return frame
+
+    def set_up_body(self):
+
+        # Body        
+        body_frame = QFrame()
+        body_frame.setFrameShape(QFrame.NoFrame)
+        body_frame.setFrameShadow(QFrame.Plain)
+        body_frame.setLineWidth(0)
+        body_frame.setMidLineWidth(0)
+        body_frame.setContentsMargins(0, 0, 0, 0)
+        body_frame.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        body = QHBoxLayout()
+        body.setContentsMargins(0, 0, 0, 0)
+        body.setSpacing(0)
+        body_frame.setLayout(body)
+
+        ##############################
+        ###### TAB VIEW ##########
+
+        # Tab Widget to add editor to
+        self.tab_view = QTabWidget()
+        self.tab_view.setContentsMargins(0, 0, 0, 0)
+        self.tab_view.setTabsClosable(True)
+        self.tab_view.setMovable(True)
+        self.tab_view.setDocumentMode(True)
+        self.tab_view.tabCloseRequested.connect(self.close_tab)
+
+        ##############################
+        ###### SIDE BAR ##########
+        
+        self.side_bar = QFrame()
+        self.side_bar.setFrameShape(QFrame.StyledPanel)
+        self.side_bar.setFrameShadow(QFrame.Plain)
+        self.side_bar.setStyleSheet(f'''
+            background-color: {self.side_bar_clr};
+        ''')   
+        side_bar_layout = QVBoxLayout()
+        side_bar_layout.setContentsMargins(5, 10, 5, 0)
+        side_bar_layout.setSpacing(0)
+        side_bar_layout.setAlignment(Qt.AlignTop | Qt.AlignCenter)
+
+        # setup labels
+        folder_label = self.get_side_bar_label("./src/icons/folder-icon-blue.svg", "folder-icon")
+        side_bar_layout.addWidget(folder_label)
+
+        self.side_bar.setLayout(side_bar_layout)
+
+
+        # split view
+        self.hsplit = QSplitter(Qt.Horizontal)
+
+        ##############################
+        ###### FILE MANAGER ##########
+
+        # frame and layout to hold tree view (file manager)
+        self.file_manager_frame = self.get_frame()
+        self.file_manager_frame.setMaximumWidth(400)
+        self.file_manager_frame.setMinimumWidth(200)
+
+        self.file_manager_layout = QVBoxLayout()
+        self.file_manager_layout.setContentsMargins(0, 0, 0, 0)
+        self.file_manager_layout.setSpacing(0)
+
+        self.file_manager = FileManager(
+            tab_view=self.tab_view, 
+            set_new_tab=self.set_new_tab,
+            main_window=self
+        )
+
+        # setup layout
+        self.file_manager_layout.addWidget(self.file_manager)   
+        self.file_manager_frame.setLayout(self.file_manager_layout)
+
+        ##############################
+        ###### SETUP WIDGETS ##########
+
+        # add tree view and tab view
+        self.hsplit.addWidget(self.file_manager_frame)
+        self.hsplit.addWidget(self.tab_view)
+
+        body.addWidget(self.side_bar)
+        body.addWidget(self.hsplit)
+
+        body_frame.setLayout(body)
+
+        self.setCentralWidget(body_frame)     
+
+    def show_dialog(self, title, msg) -> int:
+        dialog = QMessageBox(self)
+        dialog.setFont(self.window_font)
+        dialog.font().setPointSize(14)
+        dialog.setWindowTitle(title)
+        dialog.setWindowIcon(QIcon(":/icons/close-icon.svg"))
+        dialog.setText(msg)
+        dialog.setStandardButtons(QMessageBox.Yes | QMessageBox.No)
+        dialog.setDefaultButton(QMessageBox.No)
+        dialog.setIcon(QMessageBox.Warning)
+
+        # Apply your stylesheet here
+        dialog.setStyleSheet("""
+        QMessageBox {
+            background-color: #181818; 
+        }
+        QLabel {
+            color: #D3D3D3; 
+        }
+        QPushButton {
+            background-color: #444; 
+            color: #D3D3D3; 
+            border: 1px solid #555; 
+            padding: 5px; 
+        }
+        QPushButton:hover {
+            background-color:
+        }
+        """)
+
+        return dialog.exec_()
+
+    def run_current_file(self):
+        current_index = self.tab_view.currentIndex()
+        
+        # Check if there is a valid tab (current_index != -1)
+        if current_index != -1:
+            current_editor = self.tab_view.widget(current_index)
+            if isinstance(current_editor, Editor):
+                current_editor.run_code()  # Call the run_code method from the editor
+        else:
+            QMessageBox.warning(self, "No File", "No file is open to run.")
+
+
+    def close_tab(self, index):
+        editor: Editor = self.tab_view.currentWidget()
+        if editor.current_file_changed:
+            dialog = self.show_dialog(
+                "Close", f"Do you want to save the changes made to {self.current_file.name}?"
+            )
+            if dialog == QMessageBox.Yes:
+                self.save_file()
+
+        self.tab_view.removeTab(index)
+
+    def show_hide_tab(self, e, type_):
+        if self.file_manager_frame.isHidden():
+            self.file_manager_frame.show()
+        else:
+            self.file_manager_frame.hide()
+
+    def tree_view_context_menu(self, pos):
+        pass
+
+    def new_file(self):
+        self.set_new_tab(Path("untitled"), is_new_file=True)
+
+    def save_file(self):
+        if self.current_file is None and self.tab_view.count() > 0:
+            self.save_as()
+        
+        editor = self.tab_view.currentWidget()
+        self.current_file.write_text(editor.text())
+        self.statusBar().showMessage(f"Saved {self.current_file.name}", 2000)
+        editor.current_file_changed = False
+    
+    def save_as(self):
+        # save as 
+        editor = self.tab_view.currentWidget()
+        if editor is None:
+            return
+        
+        file_path = QFileDialog.getSaveFileName(self, "Save As", os.getcwd())[0]
+        if file_path == '':
+            self.statusBar().showMessage("Cancelled", 2000)
+            return 
+        path = Path(file_path)
+        path.write_text(editor.text())
+        self.tab_view.setTabText(self.tab_view.currentIndex(), path.name)
+        self.statusBar().showMessage(f"Saved {path.name}", 2000)
+        self.current_file = path
+        editor.current_file_changed = False
+
+    def open_file(self):
+        # open file
+        ops = QFileDialog.Options()
+        new_file, _ = QFileDialog.getOpenFileName(self,
+                    "Pick A File", "", "All Files (*);;Python Files (*.py)",
+                    options=ops)
+        if new_file == '':
+            self.statusBar().showMessage("Cancelled", 2000)
+            return
+        f = Path(new_file)
+        self.set_new_tab(f)
+
+    def open_folder(self):
+        new_folder = QFileDialog.getExistingDirectory(self, "Pick A Folder", "")
+        if new_folder:
+            self.file_manager.model.setRootPath(new_folder)  # Set the model's root path
+            self.file_manager.setRootIndex(self.file_manager.model.index(new_folder))  # Set the tree view's root index
+            self.statusBar().showMessage(f"Opened {new_folder}", 2000)
+
+    def copy(self):
+        editor = self.tab_view.currentWidget()
+        if editor is not None:
+            editor.copy()
+
+    def paste(self):
+        editor = self.tab_view.currentWidget()
+        if editor is not None:
+            editor.paste()
+
+    def cut(self):
+        editor = self.tab_view.currentWidget()
+        if editor is not None:
+            editor.cut()
+
+    def undo(self):
+        editor = self.tab_view.currentWidget()
+        if editor is not None:
+            editor.undo()
+
+    def redo(self):
+        editor = self.tab_view.currentWidget()
+        if editor is not None:
+            editor.redo()
+
+    def close_application(self):
+        # Show a confirmation dialog before exiting
+        dialog = self.show_dialog("Exit", "Are you sure you want to exit?")
+        if dialog == QMessageBox.Yes:
+            QApplication.quit()
+
+if __name__ == '__main__':
+    app = QApplication([])
+    window = MainWindow()
+    window.setWindowState(Qt.WindowMaximized)
+    sys.exit(app.exec())
